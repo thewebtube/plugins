30dfe6e9e570375ffc9ce956977644f7239e0245
andronedev <31452517+andronedev@users.noreply.github.com>
2022-07-18 10:36:00 +0200

    better shorts disabler (css method)

--- shorts-disabler/main.js
+++ shorts-disabler/main.js
@@ -1,1 +1,8 @@
-try{document.querySelector("div.pivot-bar-item-tab.pivot-shorts").parentElement.remove()}catch{}
+css = `
+#app > ytm-pivot-bar-renderer > ytm-pivot-bar-item-renderer:nth-child(2) {
+    display:none;
+}
+`
+var styleSheet = document.createElement("style")
+styleSheet.innerText = css
+document.head.appendChild(styleSheet)
